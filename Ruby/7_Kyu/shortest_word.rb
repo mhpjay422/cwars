b2ef4161b3e9d670c4275e95649f309fae162193
Jason Gong <jasongong83@gmail.com>
2021-06-28 17:10:56 -0400

    edit shortest word

--- Ruby/7_Kyu/shortest_word.rb
+++ Ruby/7_Kyu/shortest_word.rb
@@ -5,3 +5,15 @@
 def find_short(s)
     s.split(" ").map {|word| word.length}.min
 end
+
+def find_short(s)
+    s.split(" ").map {&:length}.min
+
+    # use &: to turn the block into a Proc. 
+end
+
+# Ruby instantiates a MySymbol object;
+# Ruby checks that there is a & and calls to_proc on this object;
+# MySymbol#to_proc returns a Proc object, that expects a parameter (element) and calls a method on it (upcase);
+# my_map iterates over the received list (['foo', 'bar']) and calls the received Proc on each element, passing it as a parameter (block.call(element));
+# The Proc then executes element.send("upcase"), that is basically the same as "foo".upcase, and will return the expected result.
