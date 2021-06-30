11ec913d653a30608b2f2ec6b162d0c3fdda98a3
Jason Gong <jasongong83@gmail.com>
2021-06-30 18:04:57 -0400

    complete valid_parenthesis

---
+++ Ruby/6_Kyu/valid_parenthesis.rb
@@ -0,0 +1,24 @@
+# Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+
+# Examples
+# "()"              =>  true
+# ")(()))"          =>  false
+# "("               =>  false
+# "(())((()())())"  =>  true
+# Constraints
+# 0 <= input.length <= 100
+
+# Along with opening (() and closing ()) parenthesis, input may contain any valid ASCII characters. Furthermore, the input string may be empty and/or not contain any parentheses at all. Do not treat other forms of brackets as parentheses (e.g. [], {}, <>).
+
+def valid_parentheses(string)
+  count = 0
+  
+  string.chars.each do |ele|
+    return false if count < 0
+
+    count += 1 if ele == "(" 
+    count -= 1 if ele == ")" 
+  end
+
+  count == 0
+end
