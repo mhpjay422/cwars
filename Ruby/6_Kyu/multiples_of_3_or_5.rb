2efbf89257795872f4c9d5bf1fe2175c8b446fe7
Jason Gong <jasongong83@gmail.com>
2021-06-30 17:45:18 -0400

    codewars error but complete multiples of 3 or 5

---
+++ Ruby/6_Kyu/multiples_of_3_or_5.rb
@@ -0,0 +1,20 @@
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+# Note: If the number is a multiple of both 3 and 5, only count it once. Also, if a number is negative, return 0(for languages that do have them)
+
+def solution(number)
+  return 0 if number < 3
+  result = 0
+  count = 3
+  
+  until count > number
+    if count % 3 == 0 || count % 5 == 0
+      result += count 
+    end
+    count += 1
+  end
+  
+  result
+end
