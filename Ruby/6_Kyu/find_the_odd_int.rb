af65a475398b2d0bd9c0b6a3fc2bcc190a38cf67
Jason Gong <jasongong83@gmail.com>
2021-06-30 18:29:16 -0400

    complete find the odd int

---
+++ Ruby/6_Kyu/find_the_odd_int.rb
@@ -0,0 +1,15 @@
+# Given an array of integers, find the one that appears an odd number of times.
+
+# There will always be only one integer that appears an odd number of times.
+
+def find_it(seq)
+  count = 0
+  sorted = seq.sort
+  
+  sorted.each_with_index do |ele, i|
+    count += 1
+    next_ele_not_same = sorted[i] != sorted[i+1] 
+    
+    return ele if count.odd? && next_ele_not_same
+  end
+end
