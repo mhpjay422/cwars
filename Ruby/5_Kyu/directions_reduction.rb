ed67c27551a876d75d19a39461425333ce0f379c
Jason Gong <jasongong83@gmail.com>
2021-07-01 16:23:18 -0400

    edit directions reduction

--- Ruby/5_Kyu/directions_reduction.rb
+++ Ruby/5_Kyu/directions_reduction.rb
@@ -61,3 +61,23 @@ def dirReduc(arr)
   
   new_arr
 end
+
+
+####################
+
+
+
+OPPOSITE = {
+  "NORTH" => "SOUTH",
+  "SOUTH" => "NORTH",
+  "EAST"  => "WEST",
+  "WEST"  => "EAST"
+}
+
+def dirReduc(arr)
+  stack = []
+  arr.each do |dir|
+    OPPOSITE[dir] == stack.last ? stack.pop : stack.push(dir)
+  end
+  stack
+end
