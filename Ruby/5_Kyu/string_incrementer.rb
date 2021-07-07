a057f664353d47bf1f245327b52aa32bb79f73ef
Jason Gong <jasongong83@gmail.com>
2021-07-07 14:07:45 -0400

    fix bug in solution

--- Ruby/5_Kyu/string_incrementer.rb
+++ Ruby/5_Kyu/string_incrementer.rb
@@ -19,7 +19,8 @@
 def increment_string(input)
   nums = "0123456789"
   return input + "1" if input == "" || !nums.include?(input[-1]) 
-  end_zero, end_nine = -1
+  end_zero = -1 
+  end_nine = -1
   
   if input[end_zero] == "0"
     num_all_zero = true
@@ -30,26 +31,28 @@ def increment_string(input)
         input[-1] = "1"
         return input
       elsif input[end_zero] == "0"
+        puts "zero"
         num_all_zero = true
       end
     end
   end
   
-  if input[end_zero] == "9"
+  if input[end_nine] == "9"
     num_all_nine = true
     while num_all_nine
       num_all_nine = false
       input[end_nine] = "0"
       end_nine -= 1
-      if input[end_nine] = "9"
+      if input[end_nine] == "9"
         num_all_nine = true
+        next
       elsif !nums.include?(input[end_nine]) 
         return input[0..end_nine] + "1" + input[end_nine + 1..-1]
       end  
-        input[end_nine] = (input[end_nine].to_i + 1).to_s
+        input[end_nine..end_nine + 1] = (input[end_nine].to_i + 1).to_s + "0"
         return input
     end
   end
-  
+
   input.succ
 end
